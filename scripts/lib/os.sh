460f0b0d852bab5cdd2452fcbd32ef8ff423c5c3
psi1703 <psingh@initse.com>
2026-05-25 11:02:59 +0400

    Update os.sh

--- scripts/lib/os.sh
+++ scripts/lib/os.sh
@@ -2,9 +2,8 @@
 # Shared functions for install-otp-relay-k8s.sh. Source this file; do not execute it directly.
 
 is_debian_family() {
-  case "$OS_ID $OS_LIKE" in
-    *debian*|*ubuntu*|*raspbian*) return 0 ;;
+  case "${OS_ID:-} ${OS_LIKE:-}" in
+    *debian*|*ubuntu*) return 0 ;;
     *) return 1 ;;
   esac
 }
-
